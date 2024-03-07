850a85c21416c4cc8be76036ac0c80c9a067cada
unknown <zelalemyohannes75@gmail.com>
2024-03-07 19:12:32 +0300

    add files

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,7 +5,19 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
+
+  const [providers, setProviders ] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    }
+
+    setProviders();
+  }, [])
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -23,7 +35,7 @@ const Nav = () => {
         {isUserLoggedIn ? (<div className="flex gap-3 md:gap-5">
           <Link href="/create-prompt" className="black_btn"> Create Post</Link>
 
-          <button type="button" className="outline_btn">Sign Out</button>
+          <button onClick={signOut} type="button" className="outline_btn">Sign Out</button>
           <Link href="/profile">
             <Image src="/assets/images/logo.svg"
                    width={37} 
@@ -32,7 +44,20 @@ const Nav = () => {
                    alt="profile"
                   />
           </Link>
-        </div>) : (<></>)}
+
+        
+        </div>) : (
+                  <>
+                   {providers && Object.values(providers).map((provider) => (
+                      <button className="blackbtn" type="button" key={provider.name} onClick={() => signIn(provider.id)}>
+                        Sign IN
+                      </button>
+                    ))}
+
+                  </>               
+        
+        
+        )}
       </div>
     </nav>
   );
