cfa937278943bd1eba0c170c38d170549b245ce3
unknown <zelalemyohannes75@gmail.com>
2024-03-07 18:35:58 +0300

    add new features

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,23 +1,41 @@
-"use client"
-import Link from 'next/link';
-import Image from 'next/image';
-import { useState, useEffect } from 'react';
-import {signIn, signOut, useSession, getProviders} from 'next-auth/react';
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
   return (
-    <nav className='flex-between w-full mb-16 pt-3'>
-        <Link href="/" className='flex gap-2 flex-center'>
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text hidden lg:block text-lg">Promptopia</p>
+      </Link>
+      {/* Mobile Navigattion */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (<div className="flex gap-3 md:gap-5">
+          <Link href="/create-prompt" className="black_btn"> Create Post</Link>
+
+          <button type="button" className="outline_btn">Sign Out</button>
+          <Link href="/profile">
             <Image src="/assets/images/logo.svg"
-             alt='Logo'
-             width={30}
-             height={30}
-             className='object-contain'
-             />
-            <p className='logo_text hidden lg:block text-lg'>Promptopia</p>
-        </Link>
+                   width={37} 
+                   height={37}
+                   className="rounded-full"
+                   alt="profile"
+                  />
+          </Link>
+        </div>) : (<></>)}
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Nav;
